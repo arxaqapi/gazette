96c37a4d0461f034e4b0e21fd4870e4b44b059b3
Tarek Kunze <45296979+arxaqapi@users.noreply.github.com>
2021-05-18 14:24:09 +0200

    fix: removed spaces and fixed import issues

--- log.h
+++ log.h
@@ -1,3 +1,6 @@
+#ifndef __H_LOG_GAZETTE__
+#define __H_LOG_GAZETTE__
+
 #include <iostream>
 
 #define ANSI_RED "\x1b[31m"
@@ -5,7 +8,7 @@
 #define ANSI_RESET "\x1b[0m"
 
 #define LOG(text) (std::cout << "[" << __TIME__ << "] [" << __func__ << ":" << __LINE__ << "] - " text << std::endl)
-
 #define LOG_ERR(text) (std::cout << ANSI_RED "[" << __TIME__ << "] [" << __func__ << ":" << __LINE__ << "] - " text ANSI_RESET << std::endl)
-
 #define LOG_OK(text) (std::cout << ANSI_GREEN "[" << __TIME__ << "] [" << __func__ << ":" << __LINE__ << "] - " text ANSI_RESET << std::endl)
+
+#endif
