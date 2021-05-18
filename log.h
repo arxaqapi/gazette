4a7d5c93dd9c52737ff5903311ab55ba93374c20
Tarek Kunze <45296979+arxaqapi@users.noreply.github.com>
2021-05-18 14:10:38 +0200

    feat: added log header file

---
+++ log.h
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#define ANSI_RED "\x1b[31m"
+#define ANSI_GREEN "\x1b[32m"
+#define ANSI_RESET "\x1b[0m"
+
+#define LOG(text) (std::cout << "[" << __TIME__ << "] [" << __func__ << ":" << __LINE__ << "] - " text << std::endl)
+
+#define LOG_ERR(text) (std::cout << ANSI_RED "[" << __TIME__ << "] [" << __func__ << ":" << __LINE__ << "] - " text ANSI_RESET << std::endl)
+
+#define LOG_OK(text) (std::cout << ANSI_GREEN "[" << __TIME__ << "] [" << __func__ << ":" << __LINE__ << "] - " text ANSI_RESET << std::endl)
